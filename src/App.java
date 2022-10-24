7bb71baae776912da64f9bfa654291c57c90da24
Roleeygit <juhaszroli2001@gmail.com>
2022-10-24 16:13:22 +0200

    +Header

--- src/App.java
+++ src/App.java
@@ -1,3 +1,13 @@
+/*
+* File: App.java
+* Author: Juhász Roland
+* Copyright: 2022, Juhász Roland
+* Group: Szoft II N
+* Date: 2022-10-24
+* Github: https://github.com/Roleeygit/
+* Licenc: GNU GPL
+*/
+
 import java.util.ArrayList;
 
 public class App {
--- src/Name.java
+++ src/Name.java
@@ -1,3 +1,13 @@
+/*
+* File: Name.java
+* Author: Juhász Roland
+* Copyright: 2022, Juhász Roland
+* Group: Szoft II N
+* Date: 2022-10-24
+* Github: https://github.com/Roleeygit/
+* Licenc: GNU GPL
+*/
+
 import java.util.ArrayList;
 
 public interface Name {
