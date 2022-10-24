87d216f3d98cc003d3f05697357f8ce12ff6be18
Roleeygit <juhaszroli2001@gmail.com>
2022-10-24 16:11:34 +0200

    Program run and no mistakes

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Készitette: Juhász Roland Szoft II N 2022-10-24");
+        getNames getNames = new getNames();
+        ArrayList<String> nameList = getNames.getNames();
+        for (String name: nameList) {
+            System.out.println(name);
+        }
     }
 }
--- src/getNames.java
+++ src/getNames.java
@@ -1,3 +1,13 @@
-public interface getNames {
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class getNames implements Name{
+
+    @Override
+    public ArrayList<String> getNames() {
+        String[] names = {"Bela", "Geza"};
+        ArrayList<String> nameList = new ArrayList<>(Arrays.asList(names));
+        return nameList;
+    }
     
 }
